e496f207e856a4e4bae88fda0e219bf33a8b3ae9
yoshicon <shreyaskgantayet908@gmail.com>
2023-06-11 06:13:34 +0530

    pls accept

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import QuestionCard from './components/QuestionCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <QuestionCard
         question="What is the capital of France?"
         answer="Paris"
         showAnswer={false}
-        onToggleAnswer={() => {this.showAnswer = !this.showAnswer}}
+        onToggleAnswer={() => {}}
         onCorrect={() => {}}
         onIncorrect={() => {}}
       />
