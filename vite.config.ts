1e20fbe01aa8563f42180ed1094f12819288dd2c
yoshicon <shreyaskgantayet908@gmail.com>
2023-06-17 04:24:09 +0530

    added base in the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
-
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : '/React-Form/'
 })
